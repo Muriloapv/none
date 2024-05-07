c1f4666b876d949a80e40626a45e5453f37353a8
Murilo Alves Pereira Varoto <66957888+Muriloapv@users.noreply.github.com>
2024-05-07 11:00:39 -0300

    Delete igao do pix directory

--- igao do pix/main.js
+++
@@ -1,8 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"></script>
-
-    const btn = document.querySelector('#btn');
-    const jsConfetti = new JSConfetti();
-
-    btn.addEventListener('click', () => {
-        jsConfetti.addConfetti()
-    })
